b7e8cccea2f4aebdad713c010d6e216898fafd7b
syncvi <sync.vi.rand@protonmail.com>
2023-09-22 16:19:26 +0200

    added directory filtering when file is not provided

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,5 +1,5 @@
 use clap::{Args, Parser, Subcommand};
-use std::{error::Error, fs};
+use std::{env, error::Error, fs};
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
 pub struct Cli {
@@ -8,7 +8,7 @@ pub struct Cli {
     query: String,
 
     /// File path for snatch to read
-    #[arg(long)]
+    #[arg(long, required = false)]
     file: String,
 
     /// Makes snatch case insensitive
@@ -26,6 +26,23 @@ pub fn run(cli: &Cli) -> Result<(), Box<dyn Error>> {
         for line in search(&cli.query, &contents) {
             println!("{line}");
         }
+    } else if cli.file.is_empty() {
+        let current_dir = env::current_dir()?;
+        let entries = fs::read_dir(current_dir)?;
+
+        entries
+            .filter_map(|entry| {
+                let entry = entry.ok()?;
+                let file_name = entry.file_name().to_string_lossy().into_owned();
+                if file_name.contains(&cli.query) {
+                    Some(file_name)
+                } else {
+                    None
+                }
+            })
+            .for_each(|matching_file_name| {
+                println!("{}", matching_file_name);
+            });
     }
     Ok(())
 }
