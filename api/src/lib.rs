981a32fda1144df250dbb764ada1325530e88e33
syncvi <sync.vi.rand@protonmail.com>
2023-09-26 21:15:16 +0200

    added a multithreded search function, work in progress

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,6 +1,12 @@
 use clap::Parser;
 use owo_colors::OwoColorize;
-use std::{env, error::Error, fs, process};
+use std::{
+    env,
+    error::Error,
+    fs, process,
+    sync::{Arc, Mutex},
+    thread,
+};
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
 pub struct Cli {
@@ -16,6 +22,10 @@ pub struct Cli {
     #[arg(short, long)]
     insensitive: bool,
 
+    /// Enables mutli-threading
+    #[arg(short, long)]
+    threading: bool,
+
     /// Prints the entire file with query highlighted
     #[arg(short, long)]
     verbose: bool,
@@ -24,7 +34,7 @@ pub struct Cli {
 pub fn run(cli: &Cli) -> Result<(), Box<dyn Error>> {
     if let Some(file) = &cli.file {
         let contents = fs::read_to_string(file)?;
-        for line in search(&cli.query, &contents, &cli.insensitive) {
+        for line in searchp(&cli.query, &contents) {
             let highlighted_line = highlighter(&line, &cli.query);
             println!("{}", highlighted_line);
         }
@@ -51,6 +61,52 @@ pub fn search<'a>(query: &str, contents: &'a str, insensitive: &bool) -> Vec<&'a
     }
     results
 }
+
+fn searchp(query: &str, contents: &str) -> Vec<String> {
+    let lines: Vec<String> = contents.lines().map(|s| s.to_string()).collect();
+    let chunk_size = (lines.len() + 3) / 4; // Divide into four roughly equal parts
+
+    let query_arc = Arc::new(query.to_string());
+
+    let results = Arc::new(Mutex::new(Vec::new()));
+
+    let mut handles = vec![];
+
+    for i in 0..4 {
+        let query_arc_clone = Arc::clone(&query_arc);
+        let lines_chunk = lines
+            .iter()
+            .skip(i * chunk_size)
+            .take(chunk_size)
+            .cloned()
+            .collect::<Vec<String>>();
+        let results_clone = Arc::clone(&results);
+
+        let handle = thread::spawn(move || {
+            let query = query_arc_clone.as_str();
+            let mut matching_lines = Vec::new();
+
+            for line in &lines_chunk {
+                if line.contains(query) {
+                    matching_lines.push(line.clone());
+                }
+            }
+
+            let mut results_lock = results_clone.lock().unwrap();
+            results_lock.extend(matching_lines);
+        });
+
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    let results_lock = results.lock().unwrap();
+    results_lock.clone()
+}
+
 pub fn dir_search(query: &str, insensitive: &bool) -> Result<(), Box<dyn Error>> {
     let current_dir = env::current_dir()?;
     let entries = fs::read_dir(current_dir)?;
