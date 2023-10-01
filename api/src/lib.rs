a47a1173df788e6b66a07a0df297e156eb187338
syncvi <sync.vi.rand@protonmail.com>
2023-10-01 12:34:23 +0200

    upgraded the multithreaded search function

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -34,9 +34,19 @@ pub struct Cli {
 pub fn run(cli: &Cli) -> Result<(), Box<dyn Error>> {
     if let Some(file) = &cli.file {
         let contents = fs::read_to_string(file)?;
-        for line in searchp(&cli.query, &contents) {
-            let highlighted_line = highlighter(&line, &cli.query);
-            println!("{}", highlighted_line);
+        match cli.threading {
+            false => {
+                for line in search(&cli.query, &contents, &cli.insensitive) {
+                    let highlighted_line = highlighter(&line, &cli.query);
+                    println!("{}", highlighted_line);
+                }
+            }
+            true => {
+                for line in searchp(&cli.query, &contents, &cli.insensitive) {
+                    let highlighted_line = highlighter(&line, &cli.query);
+                    println!("{}", highlighted_line);
+                }
+            }
         }
     } else if let None = &cli.file {
         if let Err(e) = dir_search(&cli.query, &cli.insensitive) {
@@ -62,7 +72,7 @@ pub fn search<'a>(query: &str, contents: &'a str, insensitive: &bool) -> Vec<&'a
     results
 }
 
-fn searchp(query: &str, contents: &str) -> Vec<String> {
+fn searchp(query: &str, contents: &str, insensitive: &bool) -> Vec<String> {
     let lines: Vec<String> = contents.lines().map(|s| s.to_string()).collect();
     let chunk_size = (lines.len() + 3) / 4; // Divide into four roughly equal parts
 
@@ -72,31 +82,62 @@ fn searchp(query: &str, contents: &str) -> Vec<String> {
 
     let mut handles = vec![];
 
-    for i in 0..4 {
-        let query_arc_clone = Arc::clone(&query_arc);
-        let lines_chunk = lines
-            .iter()
-            .skip(i * chunk_size)
-            .take(chunk_size)
-            .cloned()
-            .collect::<Vec<String>>();
-        let results_clone = Arc::clone(&results);
+    if *insensitive {
+        // Spawn threads for case-insensitive search
+        for i in 0..4 {
+            let query_arc_clone = Arc::clone(&query_arc);
+            let lines_chunk = lines
+                .iter()
+                .skip(i * chunk_size)
+                .take(chunk_size)
+                .cloned()
+                .collect::<Vec<String>>();
+            let results_clone = Arc::clone(&results);
 
-        let handle = thread::spawn(move || {
-            let query = query_arc_clone.as_str();
-            let mut matching_lines = Vec::new();
+            let handle = thread::spawn(move || {
+                let query = query_arc_clone.as_str();
+                let mut matching_lines = Vec::new();
 
-            for line in &lines_chunk {
-                if line.contains(query) {
-                    matching_lines.push(line.clone());
+                for line in &lines_chunk {
+                    if line.to_lowercase().contains(&query.to_lowercase()) {
+                        matching_lines.push(line.clone());
+                    }
                 }
-            }
 
-            let mut results_lock = results_clone.lock().unwrap();
-            results_lock.extend(matching_lines);
-        });
+                let mut results_lock = results_clone.lock().unwrap();
+                results_lock.extend(matching_lines);
+            });
 
-        handles.push(handle);
+            handles.push(handle);
+        }
+    } else {
+        // Spawn threads for case-sensitive search
+        for i in 0..4 {
+            let query_arc_clone = Arc::clone(&query_arc);
+            let lines_chunk = lines
+                .iter()
+                .skip(i * chunk_size)
+                .take(chunk_size)
+                .cloned()
+                .collect::<Vec<String>>();
+            let results_clone = Arc::clone(&results);
+
+            let handle = thread::spawn(move || {
+                let query = query_arc_clone.as_str();
+                let mut matching_lines = Vec::new();
+
+                for line in &lines_chunk {
+                    if line.contains(query) {
+                        matching_lines.push(line.clone());
+                    }
+                }
+
+                let mut results_lock = results_clone.lock().unwrap();
+                results_lock.extend(matching_lines);
+            });
+
+            handles.push(handle);
+        }
     }
 
     for handle in handles {
